baab6bbe1296459529382745e79125bf0ec60b3a
Afaq Ahmed <afaqahmedrajpoot18664@gmail.com>
2026-08-07 00:35:09 +0500

    JSX

---
+++ JSX.jsx
@@ -0,0 +1,17 @@
+{/* The JSX is used to combine java script and html in one palace no need to create other files the react convert it */}
+{/* For example we create a function to display the data of any thing */}
+
+function Display(){
+    const name = "Afaq Ahmed";
+    return (
+        <div>
+           <h2>Calculator created by {name}</h2>
+           <p>Hello and welcome to my first app</p>
+           <button>Click me </button>
+        </div>
+    );}
+
+    {/* In this example the we use the html but actually it is java script*/}
+
+
+
