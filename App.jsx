31f60394176af4018a97fbad051a3cc109051466
Afaq Ahmed <afaqahmedrajpoot18664@gmail.com>
2026-08-09 02:35:15 +0500

    Component Done

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Projects/App/bar";
+import Footer from "./Projects/App/footer";
+import Main  from "./Projects/App/main";
+
+function App(){
+  return  (
+     <>
+        <Navbar />
+        <Main />
+        <Footer />
+     </>
+   );
+}
